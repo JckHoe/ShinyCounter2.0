9c244f5f3083553395f3f1489f5ccd08377b6181
Jack Hoe <ongjackhoe@gmail.com>
2019-12-28 23:00:07 +0800

    [Refractor] Convert to Kotlin for Launcher

--- src/main/java/fxcounterprogram/countryui/application/CounterUiApplication.kt
+++ src/main/java/fxcounterprogram/countryui/application/CounterUiApplication.kt
@@ -1,13 +1,16 @@
-package fxcounterprogram.countryui.application;
+package fxcounterprogram.countryui.application
 
-import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 
 @SpringBootApplication
-public class CounterUiApplication {
-    public static void main (String[] args){
-        Application.launch(CounterApplication.class, args);
-//        SpringApplication.run(CounterUiApplication.class, args);
+open class CounterUiApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(CounterApplication::class.java, *args)
+            //        SpringApplication.run(CounterUiApplication.class, args);
+        }
     }
 }
